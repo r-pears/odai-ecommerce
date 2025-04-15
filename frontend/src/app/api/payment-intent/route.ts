94e8c67d175f3c3b423ddb43d16434326b322226
odai-dh <odai.dahi@hyperisland.se>
2025-04-15 13:33:58 +0200

    Gitting back to square one agin... #€&€%&€%

--- frontend/src/app/api/payment-intent/route.ts
+++ frontend/src/app/api/payment-intent/route.ts
@@ -2,7 +2,6 @@ import Stripe from 'stripe';
 import { NextResponse } from 'next/server';
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
-  apiVersion: '2022-11-15',
 });
 
 export async function POST(req: Request) {
