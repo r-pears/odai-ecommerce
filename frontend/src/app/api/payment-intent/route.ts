0f1ae2804c56a5a0f0a6c4282cdb19f9d5ce1c74
odai-dh <odai.dahi@hyperisland.se>
2025-04-16 00:06:37 +0200

    fixing some bugs to be able to deploy

--- frontend/src/app/api/payment-intent/route.ts
+++ frontend/src/app/api/payment-intent/route.ts
@@ -1,7 +1,12 @@
 import Stripe from 'stripe';
 import { NextResponse } from 'next/server';
 
-const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
+if (!process.env.STRIPE_SECRET_KEY) {
+  throw new Error('STRIPE_SECRET_KEY is not defined in environment variables');
+}
+
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
+  apiVersion: '2025-02-24.acacia', 
 });
 
 export async function POST(req: Request) {
