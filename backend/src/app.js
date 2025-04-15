563fcce8765a714709acc28076170d517ac5a309
odai-dh <odai.dahi@hyperisland.se>
2025-04-16 01:26:02 +0200

    Becuase I'm an idiot

--- backend/src/app.js
+++ backend/src/app.js
@@ -15,7 +15,7 @@ app.use(
     cors({
       origin: [
         'http://localhost:3000',
-        'https://your-netlify-site.netlify.app', 
+        'https://sellby.netlify.app', 
       ],
     })
   );
